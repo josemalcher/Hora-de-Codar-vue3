a8bcfebef7603f81cd52808ada291bf6de140ad6
José Malcher Jr <contato@josemalcher.net>
2021-09-14 11:09:04 -0300

    26 - Inserindo dados no banco

--- projetoburger/src/components/BurgerForm.vue
+++ projetoburger/src/components/BurgerForm.vue
@@ -1,5 +1,5 @@
 <template>
-<!--  <Message :msg="msg" v-show="msg" />-->
+  <!--  <Message :msg="msg" v-show="msg" />-->
   <div>
     <form id="burger-form" method="POST" @submit="createBurger">
       <div class="input-container">
@@ -52,18 +52,49 @@ export default {
       msg: null
     }
   },
-  methods:{
-    async getIngredientes(){
-      const req  = await fetch("http://localhost:3000/ingredientes")
+  methods: {
+    async getIngredientes() {
+      const req = await fetch("http://localhost:3000/ingredientes")
       const data = await req.json();
       this.paes = data.paes
       this.carnes = data.carnes
       this.opcionaisdata = data.opcionais
+    },
+    async createBurger(e) {
+      e.preventDefault();
+
+      const data = {
+        nome: this.nome,
+        carne: this.carne,
+        pao: this.pao,
+        opcionais: Array.from(this.opcionais),
+        status: "Solicitado"
+      }
+
+      const dataJson = JSON.stringify(data);
+
+      const req = await fetch("http://localhost:3000/burgers", {
+        method: "POST",
+        headers: { "Content-Type" : "application/json" },
+        body: dataJson
+      });
+
+      const res = await req.json()
+
+      console.log(res)
+
+
+      // limpar campos
+      this.nome = ""
+      this.carne = ""
+      this.pao = ""
+      this.opcionais = []
     }
   },
   mounted() {
     this.getIngredientes();
-  }
+  },
+
 }
 </script>
 
@@ -120,7 +151,7 @@ input, select {
 
 .submit-btn {
   background-color: #222;
-  color:#fcba03;
+  color: #fcba03;
   font-weight: bold;
   border: 2px solid #222;
   padding: 10px;
