25b993387e801a57eb7cc621fd37e9178fe719bd
José Malcher Jr <contato@josemalcher.net>
2021-09-13 23:19:03 -0300

    22 - Finalizando cabeçado e rodapé do projeto

--- projetoburger/src/App.vue
+++ projetoburger/src/App.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <Navbar/>
+  <Navbar :logo="logo_src" :alt="app_name"/>
   <router-view/>
   <Footer/>
 
@@ -11,12 +11,35 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 export default {
-components:{
-  Navbar, Footer
-}
+  components: {
+    Navbar, Footer
+  },
+  data(){
+    return{
+      logo_src: '/img/logo.png',
+      app_name: 'Make your Burger'
+    }
+  }
 }
 </script>
 
 <style>
+* {
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 </style>
--- projetoburger/src/components/Footer.vue
+++ projetoburger/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="footer">
-    <p>Footer</p>
+    <p>Make Your Burger &copy; 2021</p>
   </div>
 </template>
 
@@ -11,5 +11,15 @@ export default {
 </script>
 
 <style scoped>
-
+#footer {
+  height: 200px;
+  background-color: #222;
+  border-top: 4px solid #111;
+  color: #fcba03;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  bottom: 0;
+  width: 100%;
+}
 </style>
