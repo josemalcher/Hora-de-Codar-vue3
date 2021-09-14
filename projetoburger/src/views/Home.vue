b7129513651654a28957d5110251379052fa3996
José Malcher Jr <contato@josemalcher.net>
2021-09-14 09:02:25 -0300

    23 - Criando banner da aplicação

--- projetoburger/src/views/Home.vue
+++ projetoburger/src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
+  <Banner/>
   <div class="main-container">
-    <h1>Home</h1>
+    <h1>Monte o seu Burger</h1>
+    <p>Formulário</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Banner from "../components/Banner";
 export default {
   name: 'Home',
   components: {
-
+    Banner
   }
 }
 </script>
